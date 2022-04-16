f1ef2eda664358a26cbeb7b5740b9c0614b1abb3
CollinPretzel <collinpreszler@email.arizona.edu>
2022-04-16 16:57:36 -0700

    Update 2P_Proc.py

--- 2P_Proc.py
+++ 2P_Proc.py
@@ -41,17 +41,17 @@ def next_slice(ax):
     ax.index = (ax.index + 1) % volume.shape[0]
     ax.images[0].set_array(volume[ax.index])
 
-# Structure of function call: python 2P_Proc.py <filename> <> <>
+# Structure of function call: python 2P_Proc.py <filtered filename> <> <>
 
 plt.rcParams['figure.figsize'] = [10, 10]
 plt.rcParams.update({'font.size': 12})
 
 # Variables that will be used throughout the processing pipeline
-global stack, procStack
+global stack, threshStack
 global imHeight, imWidth, imSlices, xRes, yRes, zRes
 # Extract these variables from filename, idk how to access tags
 global height, width, depth
-global exw, emw, freq
+#global exw, emw, freq
 
 filename = sys.argv[1]
 
@@ -69,45 +69,21 @@ rStack = scan[1]
 
 # Parameter Extraction from filename - would like to do it from TIFF tags, but running into issues
 # Example filename - 2022-03-29_Baseline_Stack_1_lam_880nm_eom_100_power_6_75_pmt_56_size_400x400mic_pixels_510x510_freq_800_LinAvg_1_range_0mic-neg200mic_slice_1micPMT - PMT [HS_1] _C6.ome
-exw = int(filename[filename.find('nm')-3:filename.find('nm')])
 width = int(filename[filename.find('size_')+5:filename.find('size_')+8])
 height = int(filename[filename.find('mic')-3:filename.find('mic')])
 depth = int(filename[filename.find('slice_')+6:filename.find('micPMT')])
 
-## Create idealized PSF for Weiner Filter, rhodamine
-emw = 520
-args = {
-    'shape': (imWidth, imHeight), # number of samples in z and r direction
-    'dims': (200, 200), # size in z and r direction in micrometers - why not 200, and what is r?
-    'ex_wavelen': exw,
-    'em_wavelen': emw, # Conventionally 520... I think
-    'num_aperture': 0.95,
-    'refr_index': 1.35, # refraction index of ultrasound gel
-    'magnification': 20,
-    'pinhole_radius': None,
-    'pinhole_shape': 'round',
-}
-
-obsvol = psf.PSF(psf.ISOTROPIC | psf.TWOPHOTON, **args)
-psf1 = obsvol.slice(0)
-psf2 = np.flip(psf1, axis=1)
-psf3 = np.concatenate((psf2,psf1),axis=1)
-psf4 = np.flip(psf3, axis=0)
-PSF = np.concatenate((psf4,psf3),axis=0)
-
-PSF = PSF[255:765,255:765]
-
-# Apply Weiner Filter
-procStack = np.empty((imHeight, imWidth))
+# Apply Otsu Thresholding
+threshStack = np.empty((imHeight, imWidth))
 for image in rStack:
-    procImage = restoration.wiener(image, PSF, 2.8) # What is this 2.8 - balance
-    procStack = np.dstack((procStack,procImage))
+    threshImage = restoration.wiener(image, PSF, 2.8) # What is this 2.8 - balance
+    threshStack = np.dstack((threshStack,threshImage))
 
-procStack = procStack[:,:,1:imSlices+1] # removes initial empty array
+threshStack = threshStack[:,:,1:imSlices+1] # removes initial empty array
 
 # Saving process to have same orientation in ImageJ and display, might be unnecessary?
 tSave = np.transpose(procStack,(2,1,0))
 tSave = np.rot90(tSave,3,axes=(1,2))
 tSave = np.flip(tSave,2)
-outfilename = filename[0:filename.find('.ome.tif')] + '_WF_rhodamine.tif'
+outfilename = filename[0:filename.find('.ome.tif')] + '_thresh_rhodamine.tif'
 imwrite('test.tif', tSave, photometric='minisblack')
