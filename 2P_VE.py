37d3ce759079751bb7371d9a115f0321a12bdf8e
CollinPretzel <collinpreszler@email.arizona.edu>
2022-04-26 17:33:40 -0700

    Separated VE function, registration now

--- 2P_VE.py
+++ 2P_VE.py
@@ -1,5 +1,4 @@
 import sys
-import cv2
 import math
 import time
 import warnings
@@ -11,50 +10,7 @@ from skimage import color, data, restoration, exposure, io
 from skimage.morphology import disk, reconstruction
 from skimage.filters import threshold_otsu, rank
 from skimage.util import img_as_ubyte
-from skimage.measure import label, regionprops, regionprops_table
-
-### Rewrite for auto local thresholding with Otsu (tested in ImageJ)
-### Worked the best for cross-sectional analysis
-### - Needs a method to assess circularity of various labeled sections
-### - Should also have a method to assess the # of pixels and exclude capillaries
-###    and lateral vessels of course
-
-# Define functions for visualization
-def multi_slice_viewer(volume):
-    fig, ax = plt.subplots()
-    ax.volume = volume
-    ax.index = volume.shape[0] // 2
-    ax.imshow(volume[ax.index])
-    fig.canvas.mpl_connect('key_press_event', process_key)
-
-def process_key(event):
-    fig = event.canvas.figure
-    ax = fig.axes[0]
-    if event.key == 'j':
-        previous_slice(ax)
-    elif event.key == 'k':
-        next_slice(ax)
-    fig.canvas.draw()
-
-def previous_slice(ax):
-    """Go to the previous slice."""
-    volume = ax.volume
-    ax.index = (ax.index - 1) % volume.shape[0]  # wrap around using %
-    ax.images[0].set_array(volume[ax.index])
-
-def next_slice(ax):
-    """Go to the next slice."""
-    volume = ax.volume
-    ax.index = (ax.index + 1) % volume.shape[0]
-    ax.images[0].set_array(volume[ax.index])
-
-def otsuThresh(img, radius):
-    img = img.astype('uint16')
-    selem = disk(radius)
-    local_otsu = rank.otsu(img, selem)
-    # threshold_global_otsu = threshold_otsu(img)
-    # global_otsu = img >= threshold_global_otsu
-    return img >= local_otsu # Was >=
+from skimage.measure import label, regionprops
 
 def trans(img):
     tSave = np.transpose(img,(2,1,0))
@@ -62,48 +18,26 @@ def trans(img):
     tSave = np.flip(tSave,2)
     return tSave
 
-# Structure of function call: python 2P_Proc.py <filtered filename> <> <>
+# Structure of function call: python 2P_VE.py <Otsu filename> <> <>
 
 plt.rcParams['figure.figsize'] = [10, 10]
 plt.rcParams.update({'font.size': 12})
 
 warnings.filterwarnings('ignore', '.*rank.*') # Ignores warnings in Otsu thresh about bit depth
 
-# Variables that will be used throughout the processing pipeline
-global stack, threshStack
-global imHeight, imWidth, imSlices, xRes, yRes, zRes
-# Extract these variables from filename, idk how to access tags
-global height, width, depth
-#global exw, emw, freq
-
 filename = sys.argv[1]
 
-# Read in file, seems to invert it? Or the imwrite inverts it
+# Read in file
 tif = TiffFile(filename)
-scan = tif.asarray() # Imports as 'CZYX', C = 0 is
-#dStack = scan[0]
-#rStack = scan[1]
+threshStack = tif.asarray() # Imports as 'CZYX', C = 0 is
 
-[imSlices, imHeight, imWidth] = scan.shape
-
-# Transform to be XYZ, might need to flip and rotate again?
-#dStack = np.transpose(dStack, (2,1,0))
-#rStack = np.transpose(rStack, (2,1,0))
+[imSlices, imHeight, imWidth] = threshStack.shape
 
 # Parameter Extraction from filename - would like to do it from TIFF tags, but running into issues
 # Example filename - 2022-03-29_Baseline_Stack_1_lam_880nm_eom_100_power_6_75_pmt_56_size_400x400mic_pixels_510x510_freq_800_LinAvg_1_range_0mic-neg200mic_slice_1micPMT - PMT [HS_1] _C6.ome
 width = int(filename[filename.find('size_')+5:filename.find('size_')+8])
 height = int(filename[filename.find('mic')-3:filename.find('mic')])
 depth = int(filename[filename.find('slice_')+6:filename.find('micPMT')])
-
-# Apply Otsu Thresholding
-threshStack = np.empty((imHeight, imWidth))
-for image in scan:
-    threshImage = otsuThresh(image, 15)
-    threshStack = np.dstack((threshStack, threshImage))
-
-threshStack = threshStack[:,:,1:imSlices+1] # removes initial empty array
-threshStack = trans(threshStack)
 
 # Remove any excessive labels -= How to make more efficient
 # And how to use the Area masks for branching
@@ -129,13 +63,13 @@ for image in threshStack:
         circ = (4*math.pi*area)/(perimeter**2)
         if (area > 100) and (area < 500):# and (ecc < 0.78) and (circ > 0.25):
             aIndices.append(num)
-            areas.append(area)
+            #areas.append(area)
             if (ecc < 0.8):
                 eccIndices.append(num)
-                eccs.append(ecc)
+                #eccs.append(ecc)
                 if (circ > 0.15) and (circ < 1):
                     circIndices.append(num)
-                    circs.append(circ)
+                    #circs.append(circ)
     for index in aIndices:
         aMask += (labelImg==index+1).astype(int)
     for index in eccIndices:
@@ -205,14 +139,11 @@ cMasks = trans(cMasks)
 aSave = aMasks.astype('float32')
 eSave = eMasks.astype('float32')
 cSave = cMasks.astype('float32')
-tSave = threshStack.astype('float32')
 fullSave = fullMask.astype('float32')
-threshOFN = filename[0:filename.find('.ome.tif')] + '_OTSU_Mask.tif'
 areaOFN = filename[0:filename.find('.ome.tif')] + '_AREA_Mask.tif'
 eccOFN = filename[0:filename.find('.ome.tif')] + '_ECC_Mask.tif'
 circOFN = filename[0:filename.find('.ome.tif')] + '_CIRC_Mask.tif'
 vesselOFN = filename[0:filename.find('.ome.tif')] + '_FULL_Mask.tif'
-imwrite(threshOFN, tSave, photometric='minisblack')
 imwrite(areaOFN, aSave, photometric='minisblack')
 imwrite(eccOFN, eSave, photometric='minisblack')
 imwrite(circOFN, cSave, photometric='minisblack')
