3e0cf461e39ea21ecaa53172305f041145162666
CollinPretzel <collinpreszler@email.arizona.edu>
2022-08-15 09:30:04 -0700

    Update 2P_ID.py

--- 2P_ID.py
+++ 2P_ID.py
@@ -43,7 +43,7 @@ def eventID(ms):
         newSlope = ms[i]-ms[i-1]
         if(newSlope > oldSlope):
             oldSlope = newSlope
-            index = I
+            index = i
     return index
 
 
